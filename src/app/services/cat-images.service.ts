c9285b25850b11095df643045d5ea430758f8eae
dzielins42 <dzielins42@gmail.com>
2017-09-07 09:40:14 +0200

    Implement CatImagesService based on thecatapi.com

--- src/app/services/cat-images.service.ts
+++ src/app/services/cat-images.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/from';
 import 'rxjs/add/operator/repeat';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
 @Injectable()
 export class CatImagesService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getCatImages(count : number) : Observable<string> {
-    return Observable.of("http://25.media.tumblr.com/tumblr_lrgxfpNoNz1qao8ddo1_500.jpg").repeat(count);
+    return this.http.get(
+      'http://thecatapi.com/api/images/get?format=xml&size=med&results_per_page=' + count
+    )
+    .map(response => {
+      let parser = new DOMParser();
+      let xmlData = parser.parseFromString(response.text(), "application/xml");
+      console.log(
+        xmlData.documentElement.nodeName == "parsererror" ? "error while parsing" : "parsing OK"
+      );
+      return xmlData.getElementsByTagName("url");
+    })
+    .flatMap(urlNodes => Observable.from(urlNodes))
+    .map(urlNode => urlNode.textContent);
   }
 
 }
